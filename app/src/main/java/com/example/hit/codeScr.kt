6b612a1dcbd9b05771d16267b3dd9a27c539c7ff
dmitry_titov <dimatitov1428@mail.ru>
2025-05-07 16:04:14 +0700

    делаю второй экран

---
+++ app/src/main/java/com/example/hit/codeScr.kt
@@ -0,0 +1,107 @@
+package com.example.hit
+
+import android.view.Surface
+import androidx.compose.animation.core.animateSizeAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.hit.ui.theme.HitTheme
+import org.jetbrains.annotations.TestOnly
+
+
+
+
+@Composable
+fun CodeScreen(
+    navController: NavHostController,
+) {
+    var showMenu by remember { mutableStateOf(false) }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        floatingActionButton = {
+            Button(
+                onClick = { showMenu = true },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF2962FF),
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(50.dp),
+                modifier = Modifier
+                    .padding(16.dp)
+                    .height(100.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Icon(Icons.Outlined.Add, contentDescription = null)
+                    Text(text = "Add Block")
+                }
+            }
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .background(Color(0xFF7890D5)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(top = 12.dp,)
+                    .height(56.dp)
+                    .fillMaxWidth(0.9f)
+                    .background(
+                        color = Color(0xFF7943DE),
+                        shape = RoundedCornerShape(100.dp)
+                    ),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    modifier = Modifier,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF7943DE),
+                        contentColor = Color.White
+                    ),
+                    onClick = { navController.navigate(Destinations.START_SCREEN) },
+                ) {
+                    Icon(
+                        Icons.Outlined.ArrowBack,
+                        contentDescription = null,
+                    )
+                }
+
+            }
+
+        }
+    }
+}
