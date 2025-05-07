30fcbbd3385e7c5c16da354e331107a0d84dcd8e
pronamka <defender0508@gmail.com>
2025-05-07 11:48:43 +0700

    first version of the parser - builds the AST to be executed later

---
+++ app/src/main/java/com/example/hit/language/parser/Parser.kt
@@ -0,0 +1,119 @@
+package org.example.parser
+
+import org.example.parser.operations.AssignmentOperation
+import org.example.parser.operations.BinaryOperation
+import org.example.parser.operations.IOperation
+import org.example.parser.operations.UnaryOperation
+import org.example.parser.operations.ValueOperation
+import org.example.parser.operations.VariableOperation
+
+class Parser(
+    private val tokens: List<Token>,
+) {
+    private val EOF_TOKEN = Token(TokenType.EOF, "")
+    private var currentIndex = 0
+
+    fun parse(): List<IOperation> {
+        val results: MutableList<IOperation> = mutableListOf()
+        while (currentIndex < tokens.size) {
+            results.add(atBottomLevel())
+        }
+        return results
+    }
+
+    private fun atTopLevel(): IOperation {
+        val currentToken = getCurrentToken()
+        if (checkCurrentTokenType(listOf(TokenType.LEFT_BRACE))) {
+            val result = atBottomLevel()
+            checkCurrentTokenType(listOf(TokenType.RIGHT_BRACE))
+            return result
+        }
+        if (checkCurrentTokenType(listOf(TokenType.WORD))) {
+            return VariableOperation(currentToken.tokenValue)
+        }
+        if (checkCurrentTokenType(
+                listOf(
+                    TokenType.INT, TokenType.DOUBLE, TokenType.STRING
+                )
+            )
+        ) {
+            return ValueOperationFactory(currentToken).create()
+        }
+        throw RuntimeException("Something went wrong")
+    }
+
+    private fun atLevelUnary(): IOperation {
+        if (checkCurrentTokenType(UnaryOperation.availableTokenTypes)) {
+            return UnaryOperation(atTopLevel(), getCurrentToken().tokenType)
+        }
+        return atTopLevel()
+    }
+
+    private fun atLevelMultiplicationDivision(): IOperation {
+        var result = atLevelUnary()
+        while (true) {
+            val operationType = getCurrentToken()
+            if (checkCurrentTokenType(listOf(TokenType.ASTERISK, TokenType.SLASH))) {
+                result = BinaryOperation(
+                    result, atLevelUnary(),
+                    operationType.tokenType
+                )
+                continue
+            }
+            break
+        }
+        return result
+    }
+
+    private fun atLevelAdditionSubstraction(): IOperation {
+        var result = atLevelMultiplicationDivision()
+        while (true) {
+            val operationType = getCurrentToken()
+            if (checkCurrentTokenType(listOf(TokenType.PLUS, TokenType.MINUS))) {
+                result = BinaryOperation(
+                    result, atLevelMultiplicationDivision(),
+                    operationType.tokenType
+                )
+                continue
+            }
+            break
+        }
+        return result
+    }
+
+    private fun atLevelAssignment(): IOperation {
+        val currentToken = getCurrentToken()
+        if (checkCurrentTokenType(listOf(TokenType.WORD)) &&
+            checkCurrentTokenType(listOf(TokenType.EQUALS))
+        ) {
+            return AssignmentOperation(currentToken.tokenValue, atLevelAdditionSubstraction())
+        }
+        return atLevelAdditionSubstraction()
+    }
+
+    private fun atBottomLevel(): IOperation {
+        return atLevelAssignment()
+    }
+
+
+    private fun checkCurrentTokenType(targetTypes: List<TokenType>): Boolean {
+        val currentToken = getCurrentToken()
+        for (tokenType in targetTypes) {
+            if (currentToken.tokenTypeEquals(tokenType)) {
+                currentIndex++
+                return true
+            }
+        }
+        return false
+    }
+
+    fun getCurrentToken(): Token {
+        if (currentIndex >= tokens.size) return EOF_TOKEN
+        return tokens[currentIndex]
+    }
+}
+
+interface ITreeLevel {
+    val allowedTokenTypes: List<TokenType>
+    fun execute(): IOperation
+}
