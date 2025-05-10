6a43ea161641e0e5707ce43cf2044f5bf296c369
pronamka <defender0508@gmail.com>
2025-05-10 14:42:17 +0700

    enum with possible conditional operation types

---
+++ app/src/main/java/com/example/hit/language/parser/operations/OperationTypes.kt
@@ -0,0 +1,10 @@
+package com.example.hit.language.parser.operations
+
+enum class ConditionalOperationType {
+    EQUAL,
+    LESS,
+    GREATER,
+    LESS_OR_EQUAL,
+    GREATER_OR_EQUAL,
+    NOT_EQUAL
+}
