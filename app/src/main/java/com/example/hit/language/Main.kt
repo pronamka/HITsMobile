ad8fe11f6e63247b5a23309bf9b1bca2c00267fa
pronamka <defender0508@gmail.com>
2025-05-10 14:46:13 +0700

    new example to test comparison

--- app/src/main/java/com/example/hit/language/Main.kt
+++ app/src/main/java/com/example/hit/language/Main.kt
@@ -5,16 +5,15 @@ import com.example.hit.language.parser.DeclarationStatement
 import com.example.hit.language.parser.Lexer
 import com.example.hit.language.parser.Parser
 import com.example.hit.language.parser.TokenType
-import com.example.hit.language.parser.Value
-import com.example.hit.language.parser.ValueOperationFactory
 import com.example.hit.language.parser.VariableType
 import com.example.hit.language.parser.operations.BinaryOperation
 import com.example.hit.language.parser.operations.IOperation
-import com.example.hit.language.parser.operations.ValueOperation
+import com.example.hit.language.parser.operations.ConditionalOperation
+import com.example.hit.language.parser.operations.ConditionalOperationType
 
 class Main{
     fun main(){
-        val atomicValues = "2\n2\na\n10"
+        val atomicValues = "2\n2\na\n10\na\n2"
         val values = Parser(Lexer(atomicValues).tokenize()).parse()
         val program: List<IOperation> = listOf(
             DeclarationStatement(
@@ -29,9 +28,11 @@ class Main{
             ),
             BinaryOperation(
                 values[2], values[3], TokenType.ASTERISK
+            ),
+            ConditionalOperation(
+                values[4], values[5], ConditionalOperationType.NOT_EQUAL
             )
         )
-
         for (statement in program){
             println(statement.evaluate())
         }
