b6797a840d588b47e47d98845924c913694bf75e
pronamka <defender0508@gmail.com>
2025-05-10 18:16:16 +0700

    new example with loops

--- app/src/main/java/com/example/hit/language/Main.kt
+++ app/src/main/java/com/example/hit/language/Main.kt
@@ -1,24 +1,43 @@
 package com.example.hit.language
 
+import androidx.compose.material3.ProvideTextStyle
 import com.example.hit.language.parser.AssignmentStatement
+import com.example.hit.language.parser.BlockStatement
 import com.example.hit.language.parser.DeclarationStatement
+import com.example.hit.language.parser.ForLoop
+import com.example.hit.language.parser.IStatement
+import com.example.hit.language.parser.IfElseStatement
 import com.example.hit.language.parser.Lexer
 import com.example.hit.language.parser.Parser
 import com.example.hit.language.parser.TokenType
 import com.example.hit.language.parser.VariableType
 import com.example.hit.language.parser.operations.BinaryOperation
-import com.example.hit.language.parser.operations.IOperation
-import com.example.hit.language.parser.operations.ConditionalOperation
-import com.example.hit.language.parser.operations.ConditionalOperationType
+import com.example.hit.language.parser.PrintStatement
+import com.example.hit.language.parser.WhileLoop
+import com.example.hit.language.parser.operations.ConditionOperation
+import com.example.hit.language.parser.operations.ConditionOperationType
 
-class Main{
-    fun main(){
-        val atomicValues = "2\n2\na\n10\na\n2"
+class Main {
+    fun main() {
+        val atomicValues = "2\n2\na\n10\na\n2\n2.5\nb\n" +
+                "\"a is greater than 2\"\n" +
+                "1\n0\ni\n" +
+                "\"i is equal to \""
         val values = Parser(Lexer(atomicValues).tokenize()).parse()
-        val program: List<IOperation> = listOf(
+        val program: List<IStatement> = listOf(
             DeclarationStatement(
                 VariableType.INT,
                 "a"
+            ),
+            DeclarationStatement(
+                VariableType.DOUBLE,
+                "b",
+                values[0]
+            ),
+            PrintStatement(values[7]),
+            AssignmentStatement(
+                "b",
+                values[3]
             ),
             AssignmentStatement(
                 "a",
@@ -26,19 +45,104 @@ class Main{
                     values[0], values[1], TokenType.PLUS
                 ),
             ),
-            BinaryOperation(
-                values[2], values[3], TokenType.ASTERISK
+            PrintStatement(values[2]),
+            IfElseStatement(
+                listOf(
+                    Pair(
+                        ConditionOperation(values[2], values[1], ConditionOperationType.EQUAL),
+                        BlockStatement(
+                            listOf(
+                                AssignmentStatement(
+                                    "a",
+                                    BinaryOperation(
+                                        values[2], values[1], TokenType.PLUS
+                                    ),
+                                ),
+                                PrintStatement(values[2])
+                            ),
+                        ),
+                    ),
+                    Pair(
+                        ConditionOperation(values[2], values[1], ConditionOperationType.GREATER),
+                        BlockStatement(
+                            listOf(
+                                PrintStatement(values[8])
+                            ),
+                        ),
+                    ),
+                ),
+                BlockStatement(
+                    listOf(
+                        AssignmentStatement(
+                            "a",
+                            BinaryOperation(
+                                values[0], values[2], TokenType.MINUS
+                            ),
+                        ),
+                        PrintStatement(values[2])
+                    ),
+                )
             ),
-            ConditionalOperation(
-                values[4], values[5], ConditionalOperationType.NOT_EQUAL
+            WhileLoop(
+                ConditionOperation(
+                    values[2], values[3], ConditionOperationType.LESS
+                ),
+                BlockStatement(
+                    listOf(
+                        AssignmentStatement(
+                            "a",
+                            BinaryOperation(
+                                values[2],
+                                values[9],
+                                TokenType.PLUS
+                            )
+                        ),
+                        PrintStatement(
+                            values[2]
+                        )
+                    )
+                )
+            ),
+            ForLoop(
+                DeclarationStatement(
+                    VariableType.INT,
+                    "i",
+                    values[10]
+                ),
+                ConditionOperation(
+                    values[11],
+                    values[3],
+                    ConditionOperationType.LESS
+                ),
+                AssignmentStatement(
+                    "i",
+                    BinaryOperation(
+                        values[11], //i
+                        values[9], //1
+                        TokenType.PLUS
+                    ),
+                ),
+                BlockStatement(
+                    listOf(
+                        PrintStatement(
+                            values[12]
+                        ),
+                        PrintStatement(
+                            values[11]
+                        )
+                    )
+                )
             )
         )
-        for (statement in program){
-            println(statement.evaluate())
+        for (statement in program) {
+            println(statement)
+        }
+        for (statement in program) {
+            statement.evaluate()
         }
     }
 }
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     Main().main()
 }
