bee145ff8bb565c19d7d0701440e91aa218e8aed
pronamka <defender0508@gmail.com>
2025-05-10 13:04:10 +0700

    added new class Variable to allow to declare new variables without having to assign a value to them right away

--- app/src/main/java/com/example/hit/language/parser/Values.kt
+++ app/src/main/java/com/example/hit/language/parser/Values.kt
@@ -1,16 +1,36 @@
 package com.example.hit.language.parser
 
 import com.example.hit.language.parser.exceptions.IncompatibleTypesException
+import com.example.hit.language.parser.operations.IOperation
 
-interface IValue{}
+interface IValue {}
 
 abstract class Value<T>(
     val value: T
 )
 
+class Variable(
+    val type: VariableType,
+    value: IOperation? = null,
+) : Value<IOperation?>(value) {
+    fun toValue(): Value<*> {
+        val variableValue = value!!.evaluate().value.toString()
+        return when (type) {
+            VariableType.INT -> IntValue(variableValue.toInt())
+            VariableType.DOUBLE -> DoubleValue(variableValue.toDouble())
+            VariableType.STRING -> StringValue(variableValue)
+            else -> throw NotImplementedError("Variable with type $type cannot be created yet.")
+        }
+    }
+
+    override fun toString(): String{
+        return "Variable type $type and value $value"
+    }
+}
+
 abstract class SupportsArithmetic<T>(
     value: T
-): Value<T>(value) {
+) : Value<T>(value) {
     abstract fun add(other: SupportsArithmetic<*>): SupportsArithmetic<*>
     abstract fun subtract(other: SupportsArithmetic<*>): SupportsArithmetic<*>
     abstract fun multiplyBy(other: SupportsArithmetic<*>): SupportsArithmetic<*>
@@ -21,40 +41,40 @@ abstract class SupportsArithmetic<T>(
 
 class IntValue(
     value: Int
-) : SupportsArithmetic<Int>(value){
+) : SupportsArithmetic<Int>(value) {
     override fun toString(): String {
         return value.toString()
     }
 
     override fun add(other: SupportsArithmetic<*>): SupportsArithmetic<*> {
-        return when(other){
-            is IntValue -> IntValue(value+other.value)
-            is DoubleValue -> DoubleValue(value+other.value)
+        return when (other) {
+            is IntValue -> IntValue(value + other.value)
+            is DoubleValue -> DoubleValue(value + other.value)
             else -> throw IncompatibleTypesException("+", listOf(this, other))
         }
     }
 
     override fun subtract(other: SupportsArithmetic<*>): SupportsArithmetic<*> {
-        return when(other){
-            is IntValue -> IntValue(value-other.value)
-            is DoubleValue -> DoubleValue(value-other.value)
+        return when (other) {
+            is IntValue -> IntValue(value - other.value)
+            is DoubleValue -> DoubleValue(value - other.value)
             else -> throw IncompatibleTypesException("-", listOf(this, other))
         }
     }
 
     override fun multiplyBy(other: SupportsArithmetic<*>): SupportsArithmetic<*> {
-        return when(other){
-            is IntValue -> IntValue(value*other.value)
-            is DoubleValue -> DoubleValue(value*other.value)
+        return when (other) {
+            is IntValue -> IntValue(value * other.value)
+            is DoubleValue -> DoubleValue(value * other.value)
             is StringValue -> StringValue(other.value.repeat(value))
             else -> throw IncompatibleTypesException("*", listOf(this, other))
         }
     }
 
     override fun divideBy(other: SupportsArithmetic<*>): SupportsArithmetic<*> {
-        return when(other){
-            is IntValue -> IntValue(value/other.value)
-            is DoubleValue -> DoubleValue(value/other.value)
+        return when (other) {
+            is IntValue -> IntValue(value / other.value)
+            is DoubleValue -> DoubleValue(value / other.value)
             else -> throw IncompatibleTypesException("/", listOf(this, other))
         }
     }
@@ -66,39 +86,39 @@ class IntValue(
 
 class DoubleValue(
     value: Double
-): SupportsArithmetic<Double>(value) {
+) : SupportsArithmetic<Double>(value) {
     override fun toString(): String {
         return value.toString()
     }
 
     override fun add(other: SupportsArithmetic<*>): SupportsArithmetic<*> {
-        return when(other){
-            is IntValue -> DoubleValue(value+other.value)
-            is DoubleValue -> DoubleValue(value+other.value)
+        return when (other) {
+            is IntValue -> DoubleValue(value + other.value)
+            is DoubleValue -> DoubleValue(value + other.value)
             else -> throw IncompatibleTypesException("+", listOf(this, other))
         }
     }
 
     override fun subtract(other: SupportsArithmetic<*>): SupportsArithmetic<*> {
-        return when(other){
-            is IntValue -> DoubleValue(value-other.value)
-            is DoubleValue -> DoubleValue(value-other.value)
+        return when (other) {
+            is IntValue -> DoubleValue(value - other.value)
+            is DoubleValue -> DoubleValue(value - other.value)
             else -> throw IncompatibleTypesException("-", listOf(this, other))
         }
     }
 
     override fun multiplyBy(other: SupportsArithmetic<*>): SupportsArithmetic<*> {
-        return when(other){
-            is IntValue -> DoubleValue(value*other.value)
-            is DoubleValue -> DoubleValue(value*other.value)
+        return when (other) {
+            is IntValue -> DoubleValue(value * other.value)
+            is DoubleValue -> DoubleValue(value * other.value)
             else -> throw IncompatibleTypesException("*", listOf(this, other))
         }
     }
 
     override fun divideBy(other: SupportsArithmetic<*>): SupportsArithmetic<*> {
-        return when(other){
-            is IntValue -> DoubleValue(value/other.value)
-            is DoubleValue -> DoubleValue(value/other.value)
+        return when (other) {
+            is IntValue -> DoubleValue(value / other.value)
+            is DoubleValue -> DoubleValue(value / other.value)
             else -> throw IncompatibleTypesException("/", listOf(this, other))
         }
     }
@@ -114,8 +134,8 @@ class StringValue(value: String) : SupportsArithmetic<String>(value) {
     }
 
     override fun add(other: SupportsArithmetic<*>): SupportsArithmetic<*> {
-        return when(other){
-            is StringValue -> StringValue(value+other.value)
+        return when (other) {
+            is StringValue -> StringValue(value + other.value)
             else -> throw IncompatibleTypesException("+", listOf(this, other))
         }
     }
@@ -125,7 +145,7 @@ class StringValue(value: String) : SupportsArithmetic<String>(value) {
     }
 
     override fun multiplyBy(other: SupportsArithmetic<*>): SupportsArithmetic<*> {
-        return when(other){
+        return when (other) {
             is IntValue -> StringValue(value.repeat(other.value))
             else -> throw IncompatibleTypesException("*", listOf(this, other))
         }
@@ -140,6 +160,6 @@ class StringValue(value: String) : SupportsArithmetic<String>(value) {
     }
 }
 
-class BoolValue(value: Boolean): Value<Boolean>(value){
+class BoolValue(value: Boolean) : Value<Boolean>(value) {
 
 }
