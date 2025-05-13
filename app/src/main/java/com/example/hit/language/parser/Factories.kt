1c7a8f2d4f42e470578a3e89d40d13e0a389ceac
pronamka <defender0508@gmail.com>
2025-05-13 14:56:27 +0700

    refactored the logic of the ValueFactory so that it can create arrays

--- app/src/main/java/com/example/hit/language/parser/Factories.kt
+++ app/src/main/java/com/example/hit/language/parser/Factories.kt
@@ -1,21 +1,44 @@
 package com.example.hit.language.parser
 
+import com.example.hit.language.parser.exceptions.ArrayInitializationException
 import com.example.hit.language.parser.operations.ValueOperation
 
 class ValueFactory(
     val token: Token
 ) {
     fun create(): Value<*> {
-        if (token.tokenTypeEquals(TokenType.INT)) {
-            return IntValue(token.tokenValue.toInt())
+        return when (token.tokenType) {
+            TokenType.INT -> IntValue(token.tokenValue.toInt())
+            TokenType.DOUBLE -> DoubleValue(token.tokenValue.toDouble())
+            TokenType.STRING -> StringValue(token.tokenValue)
+            TokenType.ARRAY -> {
+                if (token !is ArrayToken) {
+                    throw ArrayInitializationException(
+                        "Failed to initialize array: Array can only " +
+                                "be initialized with an array expression."
+                    )
+                }
+                if (token.value.size() != token.size) {
+                    throw ArrayInitializationException(
+                        "Failed to initialize array: Array size was ${token.size} " +
+                                "but the array expression contains ${token.value.size()} elements"
+                    )
+                }
+                val elements: MutableList<Value<*>> = token.value.toList().toMutableList()
+                val desiredClass = VariableType.classMap[token.elementType]!!
+                for (element in elements) {
+                    if (!desiredClass.isInstance(element)) {
+                        throw ArrayInitializationException(
+                            "Failed to initialize array: Array element type was ${desiredClass.java.simpleName}," +
+                                    "but the element $element has different type."
+                        )
+                    }
+                }
+                return ArrayValue(token.size, desiredClass, elements)
+            }
+
+            else -> throw NotImplementedError("Token of type ${token.tokenType} cannot be parsed into a value.")
         }
-        if (token.tokenTypeEquals(TokenType.DOUBLE)) {
-            return DoubleValue(token.tokenValue.toDouble())
-        }
-        if (token.tokenTypeEquals(TokenType.STRING)) {
-            return StringValue(token.tokenValue)
-        }
-        throw NotImplementedError("Token of type ${token.tokenType} cannot be parsed into a value yet.")
     }
 }
 
@@ -24,23 +47,5 @@ class ValueOperationFactory(
 ) {
     fun create(): ValueOperation {
         return ValueOperation(ValueFactory(token).create())
-    }
-}
-
-class ArrayValueFactory(
-    val size: Int,
-    val elementsType: VariableType,
-    val values: String
-) {
-    fun create(): ArrayValue<*> {
-        val elementsStrings = values.split(",")
-        val elements: MutableList<Value<*>> = mutableListOf()
-
-        for (elementString in elementsStrings) {
-            val value =
-                Variable(elementsType, Parser(Lexer(elementString).tokenize()).parse()[0]).toValue()
-            elements.add(value)
-        }
-        return ArrayValue(size, elements)
     }
 }
