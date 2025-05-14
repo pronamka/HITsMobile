c33079588f0655bfad5bd15a04522477f5a07931
pronamka <defender0508@gmail.com>
2025-05-14 17:37:15 +0700

    added a new type: FunctionValue that inherits from abstract class CallableValue

--- app/src/main/java/com/example/hit/language/parser/Values.kt
+++ app/src/main/java/com/example/hit/language/parser/Values.kt
@@ -2,6 +2,7 @@ package com.example.hit.language.parser
 
 import com.example.hit.language.parser.exceptions.IncompatibleTypesException
 import com.example.hit.language.parser.exceptions.InvalidOperationException
+import com.example.hit.language.parser.exceptions.InvalidParametersAmountException
 import com.example.hit.language.parser.exceptions.UnexpectedTypeException
 import com.example.hit.language.parser.operations.IOperation
 import kotlin.reflect.KClass
@@ -11,6 +12,8 @@ interface IValue {}
 abstract class Value<T>(
     val value: T
 )
+
+class NullValue: Value<Any>(0)
 
 class Variable(
     val type: VariableType,
@@ -289,5 +292,36 @@ class ArrayValue<T : Value<*>>(
         return "Array Value: Size $size, Elements: [${
             stringRepresentation.toString().trimEnd().trimEnd(',')
         }]"
+    }
+}
+
+abstract class CallableValue<T>(
+    val parametersDeclarations: List<DeclarationStatement> = listOf(),
+    value: T
+) : Value<T>(value) {
+    abstract fun call(parametersValues: List<IOperation> = listOf()): Value<*>
+}
+
+class FunctionValue(
+    parametersDeclarations: List<DeclarationStatement>,
+    value: BlockStatement
+) : CallableValue<BlockStatement>(parametersDeclarations, value) {
+    override fun call(parametersValues: List<IOperation>): Value<*> {
+        if (parametersDeclarations.size != parametersValues.size) {
+            throw InvalidParametersAmountException(
+                parametersDeclarations.size,
+                parametersValues.size
+            )
+        }
+        for (i in 0..parametersDeclarations.size - 1) {
+            parametersDeclarations[i].variableValue = parametersValues[i]
+            value.addStatement(i, parametersDeclarations[i])
+        }
+        value.evaluate()
+        var returnValue = value.outputValue?.evaluate()
+        if (returnValue == null){
+            return NullValue()
+        }
+        return returnValue
     }
 }
