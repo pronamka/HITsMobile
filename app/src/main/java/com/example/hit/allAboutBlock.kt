1d960ebf470f6056c5bef007dd6b5676511b0e6a
Kirill <kmalahov703@gmail.com>
2025-05-24 21:35:20 +0700

    refactor: move logic to other files

--- app/src/main/java/com/example/hit/allAboutBlock.kt
+++ app/src/main/java/com/example/hit/allAboutBlock.kt
@@ -30,115 +30,12 @@ import com.example.hit.language.parser.operations.ReturnOperation
 import java.util.UUID
 import kotlin.uuid.Uuid
 
-enum class BlockType(val value: String) {
-
-    VARIABLE_INITIALIZATION(""),
-
-    VARIABLE_DECLARATION("variable_declaration"),
-    ARRAY_DECLARATION("array_dec"),
-
-    VARIABLE_ASSIGNMENT("variable_as"),
-    ARRAY_ELEMENT_ASSIGNMENT("array_elem_as"),
-
-    IF("if"),
-
-    FOR("for"),
-    WHILE("while"),
-
-    RETURN("return"),
-
-    PRINT("print"),
-    BREAK("break"),
-    CONTINUE("continue"),
-
-    BLOCK("block"),
-    FUNCTION("function");
-}
-
-enum class DataType {
-    INT,
-    STRING,
-    FLOAT,
-    BOOLEAN
-}
 
 data class BlockPosition(
     var id : UUID,
     var posX : Float = 0f,
     var posY : Float = 0f
 )
-
-//data class CodeBlock(
-//    val id: Int,
-//    val type: BlockType,
-//    val color: Color,
-//    val relatedBlock: BasicBlock? = null
-//)
-//
-//object BlockData {
-//    val defaultBlocks = listOf(
-//        CodeBlock(
-//            id = 1,
-//            type = BlockType.VARIABLE_INITIALIZATION,
-//            color = Color(0xFF2196F3)
-//        ),
-//        CodeBlock(
-//            id = 2,
-//            type = BlockType.IF,
-//            color = Color(0xFFA5D6A7)
-//        ),
-//        CodeBlock(
-//            id = 3,
-//            type = BlockType.ELSE,
-//            color = Color(0xFF81C784)
-//        ),
-//        CodeBlock(
-//            id = 4,
-//            type = BlockType.ELSE_IF,
-//            color = Color(0xFF66BB6A)
-//        ),
-//        CodeBlock(
-//            id = 5,
-//            type = BlockType.FOR,
-//            color = Color(0xFFFFCC80)
-//        ),
-//        CodeBlock(
-//            id = 6,
-//            type = BlockType.WHILE,
-//            color = Color(0xFFFFB74D)
-//        ),
-//        CodeBlock(
-//            id = 9,
-//            type = BlockType.NOT,
-//            color = Color(0xFF8F52FF)
-//        ),
-//        CodeBlock(
-//            id = 10,
-//            type = BlockType.RETURN,
-//            color = Color(0xFFDE30FF)
-//        ),
-//        CodeBlock(
-//            id = 11,
-//            type = BlockType.PRINT,
-//            color = Color(0xFF03A9F4)
-//        ),
-//        CodeBlock(
-//            id = 12,
-//            type = BlockType.BREAK,
-//            color = Color(0xFF2D2F2A)
-//        ),
-//        CodeBlock(
-//            id = 13,
-//            type = BlockType.FUNCTION,
-//            color = Color(0xFF257043)
-//        ),
-//        CodeBlock(
-//            id = 14,
-//            type = BlockType.CONTINUE,
-//            color = Color(0xFFCFFF00)
-//        )
-//    )
-//}
 
 
 
