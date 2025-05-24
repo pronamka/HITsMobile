ae15e77bcfe49b80c96d6ebeb518d848b4eef41e
Kirill <kmalahov703@gmail.com>
2025-05-24 21:36:44 +0700

    refactor: new code runner logic

--- app/src/main/java/com/example/hit/codeRunner/CodeRunner.kt
+++ app/src/main/java/com/example/hit/codeRunner/CodeRunner.kt
@@ -1,38 +1,23 @@
 package com.example.hit.codeRunner
+import androidx.compose.runtime.snapshots.Snapshot
 import com.example.hit.blocks.BasicBlock
 import com.example.hit.language.parser.IStatement
 
-//class CodeRunner(private val blocks : List<BasicBlock>) {
-//    fun run(){
-//        consoleOutput
-//        consoleOutput.clear()
-//        consoleOutput.add("Program execution started...")
-//        val statements = mutableListOf<IStatement>()
-//        for (block in blocks) {
-//            statements.add(block.execute())
-//        }
-//        for (statement in statements) {
-//            statement.evaluate()
-//        }
-//    }
-//}
-
-
-//    fun runProgram(){
-//        val ourBlocks = listOf<BasicBlock>()
-//        val statements = mutableListOf<IStatement>()
-//
-//        consoleOutput.clear()
-//        consoleOutput.add("Program execution started...")
-//
-//        try {
-//            for (ourBlock in ourBlocks) {
-//                val statement = ourBlock.execute()
-//                statements.add(statement)
-//                consoleOutput.add("Executed: ${statement.toString()}")
-//            }
-//            consoleOutput.add("Program execution completed successfully!")
-//        } catch (e: Exception) {
-//            consoleOutput.add("Error: ${e.message}")
-//        }
-//    }
+class CodeRunner(private val blocks : List<BasicBlock>, private val console: MutableList<String>) {
+    fun run(){
+        console.clear()
+        console.add("Program execution started...")
+        try {
+            val statements = mutableListOf<IStatement>()
+            for (block in blocks) {
+                statements.add(block.execute())
+            }
+            for (statement in statements) {
+                statement.evaluate()
+            }
+            console.add("Program execution completed successfully!")
+        } catch (e: Exception) {
+            console.add("Error: ${e.message}")
+        }
+    }
+}
